d010dbf3c069a16bf0363a1588c5c6ac54faeccd
shubhradip <shubhradipsarkar@gmail.com>
2025-04-24 09:32:06 +0530

    first commit

---
+++ src/app/management/page.jsx
@@ -0,0 +1,19 @@
+function Management() {
+    return ( 
+        <div>
+            <ul>
+            <li>Introduction</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li></ul>
+        </div>
+     );
+}
+
+export default Management;
